6f47c3752553bb473b5e978ddd3f0e31d63ecbfb
Roberto Duessmann <roberto.duessmann@gmail.com>
2019-03-12 20:12:37 +0100

    Create spring boot app

---
+++ src/test/java/com/metrics/springbootprometheus/SpringBootPrometheusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.metrics.springbootprometheus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootPrometheusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
